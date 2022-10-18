463cb8448d64bec4b285ed91057db37ea6013cf2
alpharou <alpharou.dev@gmail.com>
2022-10-19 00:37:20 +0200

    Basic camera

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 use bevy::prelude::*;
 
+pub const HEIGHT: f32 = 720.0;
+pub const WIDTH: f32 = 1280.0;
+
 fn main() {
 
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.2,0.2,0.2)))
+        .insert_resource(WindowDescriptor {
+            width: WIDTH,
+            height: HEIGHT,
+            title: "Bevy tower defense".to_string(),
+            resizable: false,
+            ..Default::default()
+        })
+        .add_startup_system(spawn_camera)
+        .add_plugins(DefaultPlugins)
+        .run();
 
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0,2.5,5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
